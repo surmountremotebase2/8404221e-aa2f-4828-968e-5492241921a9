d46310437625290d6d671c70209383ec014dd4d4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-13 12:03:47 -0400

    User Ao Chen saved changes to multiple files files at 06/13/2024, 16:03:46 UTC time.

--- a3fce7ee-6b21-49cf-b059-302da9fed8c7/main.py
+++ a3fce7ee-6b21-49cf-b059-302da9fed8c7/main.py
@@ -2,23 +2,10 @@ from surmount.base_class import Strategy, TargetAllocation
 from surmount.logging import log
 from surmount.data import Asset, InstitutionalOwnership
 import numpy as np
-import scipy
 import pandas as pd
 
-def SMAVol(ticker, data, length):
-   '''Calculate the moving average of trading volume
-
-   :param ticker: a string ticker
-   :param data: data as provided from the OHLCV data function
-   :param length: the window
-
-   :return: list with float SMA
-   '''
-   close = [i[ticker]["volume"] for i in data]
-   d = ta.sma(pd.Series(close), length=length)
-   if d is None:
-      return None
-   return d.tolist()
+def gaussian(x):
+   return np.exp(-x**2 / 2)
 
 class TradingStrategy(Strategy):
    def __init__(self):
@@ -61,12 +48,12 @@ class TradingStrategy(Strategy):
    def run(self, data):
       data = data.loc[self.tickers, "ohlcv"]
       growth = (data["close"] - data["open"]) / data["open"]
-      P0 = np.prod(scipy.stats.norm.pdf(growth / self.std.loc))
+      P0 = np.prod(gaussian(growth / self.std.loc))
 
       for group in self.groups:
          group_growth = growth[group]
          mean = np.mean(group_growth)
-         P_common = np.prod(scipy.stats.norm.pdf((group_growth - mean) / std.loc[group]))
+         P_common = np.prod(gaussian((group_growth - mean) / std.loc[group]))
          P_seperate = P0[group]
          is_up = mean > 0 and P_common > 4 * P_seperate
          is_down = mean < 0 and P_common > 4 * P_seperate
